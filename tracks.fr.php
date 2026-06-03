fc1ad3697eea3847485031dba7c8fae2f71d0a48
hoewer <90464130+hoewer@users.noreply.github.com>
2026-06-03 14:15:03 +0200

    Add files via upload
    French localization by ThibautPlg

---
+++ tracks.fr.php
@@ -0,0 +1,51 @@
+<?php
+
+$label = array(
+	'track' =>		"Piste",
+	'chooseTrack' =>	"Choisir le circuit",
+	'laps' =>		"tours",
+	'chooseLaps' =>		"Nombre de tours",
+	'direction' =>		"Direction",
+	'mode' =>		"Mode",
+	'normal' =>		"Normal",
+	'reverse' =>		"inverse",
+	'both' =>		"Les deux",
+	'time-trial' =>		"Contre-la-montre",
+	'recent' =>		"Records récents",
+	'unique' =>		"Joueurs uniques",
+	'yes' =>		"Oui",
+	'no' =>			"Non",
+	'submit' =>		"Valider",
+	'icon' =>		"Icone",
+	'user' =>		"Joueur",
+	'result' =>		"Résultat",
+	'date' =>		"Date",
+	'more' =>		"plus"
+);
+
+// Define your tracks: STK-ID, readable name, default laps
+$track = array(
+	array("abyss", "Abysses antédiluviennes", 3),
+	array("lighthouse", "Autour du phare", 4),
+	array("black_forest", "Forêt-Noire", 2),
+	array("candela_city", "La cité Candela", 3),
+	array("cocoa_temple", "Temple cacao", 3),
+	array("cornfield_crossing", "Croisée campagnarde", 3),
+	array("fortmagma", "Fort Magma", 3),
+	array("gran_paradiso_island", "L’île Grand Paradiso", 3),
+	array("hacienda", "Hacienda", 3),
+	array("minigolf", "Minigolf", 4),
+	array("scotland", "Étang de Nessie", 3),
+	array("snowmountain", "Station nordique", 3),
+	array("mines", "Vieille mine", 3),
+	array("olivermath", "Classe de maths d’Oliver", 6),
+	array("ravenbridge_mansion", "Manoir Ravenbridge", 3),
+	array("sandtrack", "Sables mouvants", 3),
+	array("snowtuxpeak", "Pic enneigé", 3),
+	array("stk_enterprise", "STK Enterprise", 3),
+	array("volcano_island", "Ile volcanique", 2),
+	array("xr591", "XR591", 3),
+	array("zengarden", "Jardin zen ", 4)
+);
+
+?>
